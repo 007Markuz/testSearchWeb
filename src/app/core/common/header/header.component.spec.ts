425b6a0651ae366c27fa04b24671586be5a06adb
mheredia <marcos.heredia@globant.com>
2020-11-06 23:49:38 -0300

    se completa el formulario del header para escuchar los cambios del form

--- src/app/core/common/header/header.component.spec.ts
+++ src/app/core/common/header/header.component.spec.ts
@@ -1,19 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { By } from '@angular/platform-browser';
+import { ProductStoreService } from '../../service/product-store.service';
+import { timer } from 'rxjs';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let service : ProductStoreService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports:[ FormsModule, ReactiveFormsModule, CommonModule],
+      providers:[
+        {provide:ProductStoreService,
+          useFactory(){ return{
+
+            loadProduct(){}
+
+        }}}
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    service = TestBed.get(ProductStoreService);
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -22,4 +38,15 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call service on change values ', ( done) => {
+    component.registerForm.get('search').setValue('123');
+    const loadProductSpy  = spyOn(service, 'loadProduct');
+    component.registerForm.updateValueAndValidity({ onlySelf:false,emitEvent:true });
+    timer(900).subscribe(i=>{
+      expect(loadProductSpy).toHaveBeenCalled();
+      done();
+    });
+  });
+
 });
--- src/app/core/common/header/header.component.ts
+++ src/app/core/common/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { ProductStoreService } from '../../service/product-store.service';
+import { switchMap, debounceTime,  } from 'rxjs/operators';
+import {FormBuilder, FormGroup, } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
 
-  ngOnInit(): void {
-  }
+  constructor(fb: FormBuilder, private productService: ProductStoreService) {
+    this.registerForm = fb.group({search: ['']});
+   }
+   ngOnInit(): void {
+     this.registerForm.controls['search'].valueChanges.pipe(
+      debounceTime(500),
+      switchMap(value => {
+        console.log(value);
+        const id = Number(value);
+        if ( Number.isInteger(id) || value > 3 ){
+          this.productService.loadProduct(value);
+        }
+       return value;
+     })
+     ).subscribe(null);
+   }
 
 }
